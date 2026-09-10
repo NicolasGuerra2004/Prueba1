7afc848012672480204a28891dd41a2f0d9b267c
Nicolas Guerra <107363708+NicolasGuerra2004@users.noreply.github.com>
2026-09-09 22:14:09 -0300

    APP FINAL

--- app/src/main/java/com/example/prueba1/MainActivity.java
+++ app/src/main/java/com/example/prueba1/MainActivity.java
@@ -1,24 +1,35 @@
 package com.example.prueba1;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerCategories;
+    private RadioGroup radioGroupPriority;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        spinnerCategories = findViewById(R.id.spinner_categories);
+        radioGroupPriority = findViewById(R.id.radioGroup_priority);
+        progressBar = findViewById(R.id.progressBar);
+
+        String[] categories = {"Trabajo", "Estudio", "Personal"};
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, categories);
+        spinnerCategories.setAdapter(spinnerAdapter);
+
+        radioGroupPriority.setOnCheckedChangeListener((group, checkedId) -> {
+            progressBar.setProgress(progressBar.getProgress() + 10);
+            Toast.makeText(MainActivity.this, "Prioridad actualizada", Toast.LENGTH_SHORT).show();
         });
     }
 }
